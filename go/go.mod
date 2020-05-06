25b49e877fac994a29e98093ee99e9d63f3e8934
Chikage <t-9@atx.tokyo>
2020-05-06 10:24:21 +0900

    テーブル作成

---
+++ go/go.mod
@@ -0,0 +1,9 @@
+module github.com/t-9/jingu-character-service/go
+
+go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang-migrate/migrate/v4 v4.11.0
+	github.com/gorilla/mux v1.7.4
+)
--- go/main.go
+++ go/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -18,21 +21,20 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func dbHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("url path is " + r.URL.Path[1:] + "\n"))
 
-	var dbConnectQuery string
-	dbConnectQuery = "root:" + os.Getenv("DB_PASSWORD") + "@tcp(" +
-		os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
+	dbConnectQuery := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") +
+		"@tcp(" + os.Getenv("DB_HOST") + ":3306)/" + os.Getenv("DB_NAME")
 	db, err := sql.Open("mysql", dbConnectQuery)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM user") //
+	rows, err := db.Query("SELECT * FROM user")
 	if err != nil {
 		panic(err.Error())
 	}
 
-	columns, err := rows.Columns() // カラム名を取得
+	columns, err := rows.Columns()
 	if err != nil {
 		panic(err.Error())
 	}
@@ -64,11 +66,99 @@ func dbHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
+func dbCreateHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open(
+		os.Getenv("DB_DRIVER"),
+		fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/mysql",
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+		),
+	)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			w.Write([]byte(err.Error()))
+		}
+	}()
 
+	_, err = db.Exec(
+		fmt.Sprintf(
+			"CREATE DATABASE IF NOT EXISTS %s",
+			os.Getenv("DB_NAME"),
+		),
+	)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte("Successfully created database.."))
+}
+
+func dbMigrateUpHandler(w http.ResponseWriter, r *http.Request) {
+	m, err := migrate.New(
+		"file://./db/migrations/",
+		fmt.Sprintf(
+			"%s://%s:%s@tcp(%s:%s)/%s",
+			os.Getenv("DB_DRIVER"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_NAME"),
+		),
+	)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = m.Up()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fdsfs
+	w.Write([]byte("Successfully up migration version"))
+}
+
+func dbMigrateDownHandler(w http.ResponseWriter, r *http.Request) {
+	m, err := migrate.New(
+		"file://./db/migrations/",
+		fmt.Sprintf(
+			"%s://%s:%s@tcp(%s:%s)/%s",
+			os.Getenv("DB_DRIVER"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_NAME"),
+		),
+	)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	err = m.Down()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte("Successfully up migration version"))
+}
+
+func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootHandler)
 	r.HandleFunc("/db", dbHandler)
+	r.HandleFunc("/db/create", dbCreateHandler)
+	r.HandleFunc("/db/migrate/up", dbMigrateUpHandler)
+	r.HandleFunc("/db/migrate/down", dbMigrateDownHandler)
 
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
